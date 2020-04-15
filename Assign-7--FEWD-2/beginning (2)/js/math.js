84e1e4b9055b32641bb9b0fa099293033533fd54
kkuzie <kkuzii@yahoo.it>
2020-04-15 14:40:19 -0700

    Math part COMPLETE

--- Assign-7--FEWD-2/beginning (2)/js/math.js
+++ Assign-7--FEWD-2/beginning (2)/js/math.js
@@ -1,17 +1,44 @@
 /*eslint-env browser*/
 
-//STEP 1
+//STEP 1 DONE
+// Create an application that prompts the user for a number. 
+// Regardless of whether they enter a negative or positive number, make sure to display the positive version of that number in the console window. 
+// If I enter -15 in the prompt and -15 is displayed in the console window, you did this one wrong
 
-//STEP 2
+let number = prompt('Step 1 - enter a positive or negative number');
+console.log("Step 1: " + Math.abs(number));
 
-//STEP 3
+//STEP 2 DONE
+// Create an application that prompts the user for a floating point value (decimal). 
+// Store the result of that input in a variable and then round it up to the nearest whole number. 
+// Display the result within a console window.
 
-//STEP 4
-// step4: if the "string" of numbers already converted to array, just: Math.max(...arrNumbers);
-// let strNumbers = window.prompt("Enter 5 numbers separated by commas:");
-// let arrNumbers = strNumbers.trim().split(",").map(Number);
-//  Math.max(...arrNumbers);
-// that should work...
+let decimal = prompt('Step 2 - enter number with a decimal');
+console.log("Step 2: " + Math.ceil(decimal));
 
+//STEP 3 DONE
+// Create an application that prompts the user for a floating point value (decimal). 
+// Store the result of that input in a variable and then round it down to the nearest whole number. 
+// Display the result within a console window.
 
-//STEP 5
+let point = prompt("Step 3 - enter another number with a decimal");
+console.log("Step 3: " + Math.floor(point));
+
+//STEP 4 DONE
+// Create an application that prompts the user for 5 numbers. 
+// Ask them to comma delimit each number so you get 1,2,3,4,5 for example. 
+// Store the result of that input in a variable and then find the largest and smallest numbers in that list. 
+// Display both of those numbers within a console window.
+
+let five = prompt('Step 4 - enter five numbers separated by comma (1,2,3)');
+let fiveSplit = five.split(",");
+let fiveArray = Array.from(fiveSplit );
+console.log(fiveArray);
+console.log("Step 4 - " + "largest number: " + Math.max.apply(null, fiveArray) + " smallest number: " + Math.min.apply(null, fiveArray));
+
+//STEP 5 DONE
+// Create an application that prompts the user for a number. 
+// Now find the square root of that number and display the result within a console window. 
+
+let sqroot = prompt('Step 5 - enter a number');
+console.log("Step 5: " + Math.sqrt(sqroot));
